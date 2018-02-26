bb812917c11746a0e3333fa27c89ee523fc2ebcd
Mohamed El Banani <mbanani@umich.edu>
2018-02-26 17:46:24 -0500

    changed Logger's name

--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,7 @@
-from adviser_loss   import adviser_loss
-from Logger         import Logger
-from metrics        import adviser_metrics
+from tf_logger      import tf_logger
+from metrics        import adviser_metrics, vp_metrics
 
 from load_datasets  import *
+
+from adviser_loss   import adviser_loss
+from viewpoint_loss import ViewpointLoss
