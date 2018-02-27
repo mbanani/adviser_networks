464ee3177d002d922fea48c30cff6412111bc8d9
Mohamed El Banani <mbanani@umich.edu>
2018-02-27 09:35:48 -0500

    environment for PyTorch 4 -- till they make the release

---
+++ set_env_4.sh
@@ -0,0 +1,8 @@
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+module load cuda/8.0.61 cudnn/8.0-v6.0
+# PyTorch
+# module load pytorch/0.2.0
+# tensorflow for TensorBoard
+module load numpy/1.12.0 gflags/2.2.1 tensorflow/1.0.0
+module rm   numpy/1.12.0
