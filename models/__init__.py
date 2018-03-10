578bbe589ed1e8fb7ab0ea077b18391c6abec7f2
Mohamed El Banani <mbanani@umich.edu>
2018-03-10 16:11:57 -0500

    added alexBird and ch_alexBird to models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from .clickhere_cnn         import *
 from .ch_convAtt            import *
 from .alexAdviser           import *
+
+from .alexBird              import *
+from .ch_alexBird           import *
